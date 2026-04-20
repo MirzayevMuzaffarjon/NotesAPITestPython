02aa003631ea1effafc1122c7a0a8920c1068aea
MirzayevMuzaffar <mirzayevmuzaffar525@gmail.com>
2026-04-20 22:45:54 +0500

    latest changes

--- api_test/data/users.py
+++ api_test/data/users.py
@@ -1,5 +1,11 @@
 class UsersData:
+
     @staticmethod
     def get_payload_to_login(email: str, password: str):
         payload = {"email": email, "password": password}
+        return payload
+
+    @staticmethod
+    def get_payload_to_register(name: str, email: str, password: str):
+        payload = {"name": name, "email": email, "password": password}
         return payload
--- api_test/tests/test_users/test_users_register.py
+++ api_test/tests/test_users/test_users_register.py
@@ -1,3 +1,4 @@
-def test_register_valid_user(client, config):
+def test_register_valid_user(client, config, users_data):
     method = "POST"
     url = f"{config.base_url}/users/register"
+    payload = users_data.get_payload_to_register(name="Name Test auto", email=config.un_auth_email, password="Password")
